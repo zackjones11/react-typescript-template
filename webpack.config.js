512cad3da02686356946a4b286cf148076dc13e1
Zack Jones <zackjones11@gmail.com>
2020-07-25 23:12:47 +0200

    implements TypeScript

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,30 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  mode: "development",
+  entry: {
+    app: path.join(__dirname, "src", "index.tsx"),
+  },
   output: {
-    path: path.resolve("dist"),
-    filename: "main.js",
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
   },
   module: {
     rules: [
-      { test: /\.jsx?$/, loader: "babel-loader", exclude: /node_modules/ },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "index.html"),
+    }),
+  ],
 };
